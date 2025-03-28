54458f7a40c77a2c01d60f3248f09df9df5fb9f6
yan3ku <wisniewskijas@outlook.com>
2025-03-28 07:21:10 +0100

    Scrape the site (graph not weighted yet)

--- tczew-komunikacja.lisp
+++ tczew-komunikacja.lisp
@@ -4,6 +4,7 @@
 
 
 (defparameter *url-root* "https://komunikacja.tczew.pl/")
+(defparameter *stations* (make-hash-table :test 'equal))
 
 (defclass root-request (scrapycl:request)
   ())
@@ -17,14 +18,36 @@
     	      :reader bus-dir)))
 
 (defclass station-request (bus-line-request)
-  ((station :initarg :station
-	    :type string
-	    :reader bus-station)))
+  ((name :initarg :name
+	 :type string
+	 :reader name)
+   (departure :initarg :departure
+	      :accessor departure)))
 
 (defclass bus-lines-spider (scrapycl:spider)
   ()
   (:default-initargs
    :initial-requests (list (make-instance 'root-request :url *url-root*))))
+
+
+(defclass station ()
+  ((cost-to :initarg :cost-to
+	    :documentation "A list of costs to other stations."
+	    :type list)
+   (name :initarg :station
+	 :type string)
+   (departures :initarg :departures
+	       :reader departures
+	       :type list)))
+
+(defclass station-departure ()
+  ((dest :initarg :dest
+	 :type station
+	 :reader dest)
+   (dtime :initarg :dtime
+	  :type string
+	  :reader dtime)))
+
 
 (defun make-bus-line-req (page direction)
   (make-instance 'bus-line-request
@@ -32,25 +55,22 @@
     		 :line-nr (parse-integer (cl-ppcre:scan-to-strings "\\d+" page))
     		 :direction (str:trim direction)))
 
-(defun make-station-req (bus-line-req station-name station-page)
+(defun make-station-req (&key bus name page)
   (make-instance 'station-request
-    		 :url station-page
-    		 :station station-name
-    		 :line-nr (bus-nr bus-line-req)
-    		 :direction (bus-dir bus-line-req)))
+    		 :url page
+    		 :name name
+    		 :line-nr (bus-nr bus)
+    		 :direction (bus-dir bus)))
 
 (defmethod print-object ((obj station-request) stream)
-  (print-unreadable-object (obj stream :type t)
-    (format stream "STATION ~a "
-	    (bus-station obj)))
+  (format stream "\"~a\"~C" (name obj) #\Tab)
   (call-next-method))
 
 (defmethod print-object ((obj bus-line-request) stream)
-  (print-unreadable-object (obj stream :type t)
-    (format stream "NR ~A URL ~A DIR ~A"
-	    (bus-nr obj)
-	    (scrapycl:request-url obj)
-	    (bus-dir obj))))
+  (format stream "[~A] href:~A to [~A]"
+	  (bus-nr obj)
+	  (scrapycl:request-url obj)
+	  (bus-dir obj)))
 
 (defun parse-root-to-stations-req (lines-req)
   "Extract from root page url of bus lines route tables"
@@ -64,12 +84,6 @@
 
 ;; kurs url
 ;; kurs.php?kat=001_20250301&kier=1&nr=1&kurs=2&a=1
-(defmethod scrapycl:process ((spider bus-lines-spider)
-    			     (request root-request))
-  (multiple-value-bind (data url) (scrapycl:fetch spider request)
-    (declare (ignore url))
-    (print (coerce (parse-root-to-stations-req data) 'list))))
-
 
 (defun get-route-table-stations (bus-req table)
   "Extract from route table station name and the url for its time table"
@@ -78,43 +92,66 @@
 	     (text)
 	     (lquery:$1 (attr :href) (merge-url-with *url-root*)))
 	    (map-apply (lambda (name href)
-			 (make-station-req bus-req name href)))))
+			 (make-station-req
+			  :bus bus-req
+			  :name name
+			  :page href)))))
+
+(defun get-last-station-name (table)
+  ;; trim the ^
+  (str:trim (lquery:$1 table "tr" (last) "[style*=\"font-weight: bold;\"]" (text))))
+
+
+;;; eq 1 this is the table from the 3 tables
+(defun row-to-time (tr)
+  (let* ((hour (lquery:$1 tr "td" (first) (text)))
+	 (minute-divs (lquery:$ tr "td" (eq 1) "div")))
+    (when (> (length minute-divs) 0)	; some rows are empty
+      (when (= (length hour) 1)		; pad with 0
+	(setf hour (str:concat "0" hour)))
+      (loop for minutes across (lquery:$ minute-divs (map (lambda (el) (lquery:$1 el (text)))))
+	    collect (str:concat hour ":" minutes)))))
+
+
+(defmethod scrapycl:process ((spider bus-lines-spider)
+    			     (request root-request))
+  (multiple-value-bind (data url) (scrapycl:fetch spider request)
+    (declare (ignore url))
+    ;; limit to one bus line
+    (parse-root-to-stations-req data)))
+
 
 (defmethod scrapycl:process ((spider bus-lines-spider)
 			     (bus-req bus-line-request))
   (multiple-value-bind (data url) (scrapycl:fetch spider bus-req)
     (declare (ignore url))
-    (let* ((route-tables (lquery:$ (initialize data) ".route" (gt 1))))
-      ;; for now only the first route table
-      ;; the second one is in the opposite direction
-      (list (first (coerce (get-route-table-stations bus-req (aref route-tables 0)) 'list))))))
+    ;; for now only the first route table
+    ;; the second one is in the opposite direction
+    (let* ((route-tables (lquery:$ (initialize data) ".route" (gt 1)))
+	   (table (aref route-tables 0))
+	   (stations (get-route-table-stations bus-req table))
+	   (last-station (get-last-station-name table)))
+      (loop
+	for prev-st = st
+	for st across stations
+	unless (null prev-st)
+	  do (setf (departure prev-st) (name st))
+	finally (setf (departure st) last-station))
+      stations)))
 
-
-(defclass station ()
-  ((station :initarg :station :accessor station)))
-
-(defmethod print-object ((obj station) stream)
-  (print-unreadable-object (obj stream :type t)
-    (format stream "station: ~A" (station obj))))
-
-
-(defun row-to-time (tr)
-  (let* ((hour (lquery:$1 tr "td" (first) (text)))
-	 (minutes-in-row (lquery:$ tr "td" (eq 1) "div" (map (lambda (el) (lquery:$1 el (text)))))))
-    (when (= (length hour) 1)		; pad with 0
-      (setf hour (str:concat "0" hour)))
-    (loop for minutes across minutes-in-row
-	  collect (str:concat hour ":" minutes))))
 
 (defun get-departure-time (time-table)
+  ;; first tr is the name of the "three-table" table
   (alexandria:flatten (coerce (lquery:$ time-table "tr" (gt 1) (map #'row-to-time)) 'list)))
 
 
 (defmethod scrapycl:process ((spider bus-lines-spider)
-			     (st-req station-request))
-  (multiple-value-bind (data url) (scrapycl:fetch spider st-req)
+			     (station-req station-request))
+  (multiple-value-bind (data url) (scrapycl:fetch spider station-req)
     (log:info url)
-    (log:info (get-departure-time (lquery:$ (initialize data) ".table-three")))
+    (let ((table (get-departure-time (lquery:$ (initialize data) ".table-three"))))
+      (push (cons (cons (departure station-req) (bus-nr station-req)) table)
+	    (gethash (name station-req) *stations*)))
     (values)))
 
 (scrapycl:start (make-instance 'bus-lines-spider) :wait t)
